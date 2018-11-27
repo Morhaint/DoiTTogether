d8639b8883fb3c38725a08e0ee552444ed575b0e
Michael <44526839+Morhaint@users.noreply.github.com>
2018-11-27 22:08:25 +0300

    Add files via upload
    Start project

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Я хочу!</title>
+    <meta name="description" content="Заполнить"/>
+    <meta http-equiv="Content-Language" content="ru">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" type="text/css" charset="utf-8" />
+    <link rel="icon" href="favicon.ico" type="image/x-icon" />
+</head>
+<body>
+<div class="header">header</div>
+<br/>
+<div class="wrapper">
+    <div class="sidebar">sidebar</div>
+    <div class="content">
+        <hr/><br/>
+        <div class="main">
+            <div class="avatar">
+                <img src="image/image1.jpg"/>
+            </div>
+            <div class="info">
+            <div class="name">
+                Ivan Petrov
+            </div>
+            <div class="city">
+                Moscow
+            </div>
+            <div class="want">
+                <span>Я хочу</span> играть на гитаре!
+            </div>
+            </div>
+                    <button class="button"><a href="#">Я тоже!</a></button>
+            </div>
+        </div>
+    </div>
+
+
+
+
+</div>
+<br/>
+<div class="footer">footer</div>
+</body>
+</html>
